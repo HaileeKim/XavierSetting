7f29cb86d97b3caff337a26f7f21b87b87e22e60
HaileeKim <0206dbsh@gmail.com>
2022-03-24 20:09:42 +0900

    Add Submodule

---
+++ settingXavier/downRTOD.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/HaileeKim/buildOpenCVXavier.git
+cd buildOpenCVXavier
+./buildOpenCV.sh
+
+cd
+git clone https://github.com/HaileeKim/r-tod.git
+cd r-tod
+make
+
---
+++ test/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+vi test.txt
+
+chmod +x test.txt
+echo "hi"
